4b8d7e9ae63b66e751245f8893975e497dcb2ef2
Lucas Augusto Souza da Silva <lucasaugusto.bel@gmail.com>
2025-05-26 23:08:18 -0300

    Ajustes de Cadastro

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
+app.secret_key = "hvxivg"
+
+
+
 @app.route("/")
-def home():
+def inicio():
     return render_template("index.html")
 
 @app.route("/cadastro", methods=["GET", "POST"])
-def cadastro():
+def cadastrar():
     if request.method == "POST":
         nome = request.form["nome"]
         data_nascimento = request.form["data_nascimento"]
@@ -16,11 +20,34 @@ def cadastro():
         telefone = request.form["telefone"]
         endereco = request.form["endereco"]
 
-        # email, tipo sanguíneo, alergias conhecidas
+        # TODO: email, tipo sanguíneo, alergias conhecidas
+
+        # TODO: passar pro banco de dados
+        # TODO: criar ID ao passar pra lá
+
+        # TODO: Se CPF já cadastrado: sistema alerta sobre duplicidade (quando tiver o banco)
+
+        # TODO: Requisitos de Qualidade:
+            # Validação de formato e dígitos verificadores de CPF
+            # Validação de dados obrigatórios (fazer no HTML)
+
+        flash("Cadastro realizado com sucesso!")
         
-        return redirect(url_for("home"))
+        return redirect(url_for("cadastrar"))
     
     return render_template("cadastro.html")
+
+@app.route("/busca", methods=["GET", "POST"])
+def buscar():
+    # TODO consultar_dados
+    # TODO atualizar_dados
+    # TODO inativar_cadastro
+    
+    return render_template("busca.html")
+
+@app.route("/agendamento", methods=["GET", "POST"])
+def agendar_consulta():
+    return render_template("agendamento.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
